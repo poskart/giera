9bdbf1550966559da9cbecfc61012fcd5669183a
poskart <piotrpos93@gmail.com>
2015-12-08 00:15:17 +0100

    New Commit after reinitialize in Visual Studio

---
+++ Game.cpp
@@ -0,0 +1,9 @@
+#include "Game.h"
+#include "Board.h"
+
+Game::Game(){}
+
+Game::~Game()
+{
+	// TODO Auto-generated destructor stub
+}
---
+++ Game.h
@@ -0,0 +1,19 @@
+/*
+* Game.h
+*
+*  Created on: 7 gru 2015
+*      Author: piotr
+*/
+
+#ifndef GAME_H_
+#define GAME_H_
+
+class Game
+{
+	Game();
+	~Game();
+private:
+	Board plansza;
+};
+
+#endif /* GAME_H_ */
